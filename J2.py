61b1ffc095be32a4db87d2bff9d2d6f6fb784495
Nachiket <nacpatil@gmail.com>
2020-06-07 06:01:42 -0700

    enrichment calcuation added

--- J2.py
+++ J2.py
@@ -1,35 +1,69 @@
-#print("Hello")
+from dict import chrdict
 import numpy as np
 import os
 import pandas as pd
 import time
+import math
+from sys import getsizeof
+import matplotlib.pyplot as plt
+import os
+import psutil
+
+print(chrdict) 
+
 start_time = time.time()
 
+rows = 6.5e6
+skip = np.arange(rows)
+skip = np.delete(skip, np.arange(0, rows, 1))
+
+
 bed_file="JT_MmSc_IgGi_0120.bed"
+window=40
 
- 
-#a = np.loadtxt(bed_file, dtype=np.str, skiprows=1, usecols=(0,1,2))
-b=pd.read_table(bed_file, delim_whitespace=True, skiprows=0, usecols=(0,1,2),engine='c',nrows=10)
+b=pd.read_table(bed_file, delim_whitespace=True, skiprows=skip, usecols=(0,1,2),engine='c')#   ,nrows=10)
 b.columns = ['Chr', 'Start', 'End']
+b.Chr=b['Chr'].map(chrdict)       # note: if the dictionary does not exhaustively map all
 
-
-print(b.shape)
 print("--- %s seconds ---" % (time.time() - start_time))
 
-convert_dict = {'Chr': str, 
+convert_dict = {'Chr': int, 
                 'Start': int,
                 'End' : int
                } 
-b.sort_values(by=['Start'])
 b = b.astype(convert_dict) 
-print(b.dtypes)
-print(b.Start)
- 
+
+
+b.sort_values(by=['Chr','Start'])
+n_rows=len(b.index)
+max_end=b['End'].max()
+
+num_bins=int(np.ceil(max_end/window))
+
+bin_enrich=np.zeros([num_bins,],dtype=int)
+enrich=np.zeros([max_end*22,1],dtype=int)
+
+
+a=b.Start.to_numpy()
+a1=b.End.to_numpy()
+chr_i=b.Chr.to_numpy()
+
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
 
+print(b.Chr.unique())
+for i in range(0,n_rows):#n_rows loop
+	pad=(chr_i[i]-1)*max_end
+	st=a[i]+pad
+	en=a1[i]+pad
+	enrich[st:en,0]+=1
+	
 
+print("--- %s seconds ---" % (time.time() - start_time))
 
+#plt.plot(enrich)
+#plt.show()
 
 
